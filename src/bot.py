373865c9ef6718bdc17233473d871ed03180654b
CyR1en <cyrien3519@gmail.com>
2020-04-04 16:30:45 -0600

    Improved logging and initial token handling

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import discord.ext
 from discord.ext import commands
@@ -9,25 +10,32 @@ from src.configuration import ConfigFile, ConfigNode
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+fmt = logging.Formatter('[%(levelname)s][%(asctime)s][%(name)s]: %(message)s')
+handler.setFormatter(fmt)
+s_handler = logging.StreamHandler()
+s_handler.setFormatter(fmt)
 logger.addHandler(handler)
+logger.addHandler(s_handler)
 
 config_file = ConfigFile("config")
 
-bot = commands.Bot(command_prefix=config_file.get_node(ConfigNode.PREFIX))
+bot = commands.Bot(command_prefix=config_file.get_string_node(ConfigNode.PREFIX))
 bot.add_cog(General(bot, config_file))
 
-token = config_file.get_node(ConfigNode.TOKEN)
-if token == ConfigNode.TOKEN.value[1]:
-    token = input("The config file is either newly generated or the token was left to its default value. \n"
-                  "Please enter your bot's token:")
-    config_file.set(ConfigNode.TOKEN, token)
+token = config_file.get_string_node(ConfigNode.TOKEN)
+if token == ConfigNode.TOKEN.get_value():
+    logger.warning("The config file is either newly generated or the token was left to its default value. \n"
+                   "Please enter your bot's token:")
+    try:
+        token = input()
+        config_file.set(ConfigNode.TOKEN, token)
+    except KeyboardInterrupt:
+        logger.error("Interrupted token input")
+        sys.exit()
 
 
 @bot.event
 async def on_ready():
-    print('Successfully logged in as {}'.format(bot.user))
+    logger.info('Successfully logged in as {}'.format(bot.user))
 
 bot.run(token)
-
-
